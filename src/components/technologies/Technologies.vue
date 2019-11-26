ec55dd4be76d39b91383180e1e004196ba5f106a
Andrew Smith <andrew.smith@rolleragency.co.uk>
2019-11-26 16:19:53 +0000

    skills

--- src/components/technologies/Technologies.vue
+++ src/components/technologies/Technologies.vue
@@ -1,6 +1,11 @@
 <template>
   <section class="technologies">
-   wegeh
+      <g-image background="transparent" fit="contain"  width="90" height="90" src="~/assets/images/technologies/node.png" alt="Node.js Logo" />
+      <g-image background="white" fit="contain" width="90" height="90" src="~/assets/images/technologies/react.png" alt="react.js Logo" />
+      <g-image background="transparent" fit="contain" width="90" height="90" src="~/assets/images/technologies/vue.png" alt="Vue.js Logo" />
+      <g-image background="transparent" fit="contain"  width="90" height="90" src="~/assets/images/technologies/typescript.png" alt="Typescript Logo" />
+      <g-image background="transparent" fit="contain"  width="90" height="90" src="~/assets/images/technologies/mongodb.png" alt="MongoDB Logo" />
+      <g-image background="transparent" fit="contain"  width="90" height="90" src="~/assets/images/technologies/php.png" alt="PHP Logo" />
   </section>
 </template>
 
