a4bd417b2463d7fa08f983577b355159bee666ac
Andrew Smith <andrew.smith@rolleragency.co.uk>
2019-11-26 16:27:15 +0000

    logos

--- src/components/technologies/Technologies.vue
+++ src/components/technologies/Technologies.vue
@@ -1,11 +1,13 @@
 <template>
   <section class="technologies">
-      <g-image background="transparent" fit="contain"  width="90" height="90" src="~/assets/images/technologies/node.png" alt="Node.js Logo" />
+      <g-image background="transparent" fit="contain"  width="90" height="90" src="~/assets/images/technologies/php.png" alt="PHP Logo" />
+      <g-image background="transparent" fit="contain"  width="90" height="90" src="~/assets/images/technologies/mongodb.png" alt="MongoDB Logo" />
       <g-image background="white" fit="contain" width="90" height="90" src="~/assets/images/technologies/react.png" alt="react.js Logo" />
+      <g-image background="white" fit="contain"  width="90" height="90" src="~/assets/images/technologies/node.png" alt="Node.js Logo" />
       <g-image background="transparent" fit="contain" width="90" height="90" src="~/assets/images/technologies/vue.png" alt="Vue.js Logo" />
       <g-image background="transparent" fit="contain"  width="90" height="90" src="~/assets/images/technologies/typescript.png" alt="Typescript Logo" />
-      <g-image background="transparent" fit="contain"  width="90" height="90" src="~/assets/images/technologies/mongodb.png" alt="MongoDB Logo" />
-      <g-image background="transparent" fit="contain"  width="90" height="90" src="~/assets/images/technologies/php.png" alt="PHP Logo" />
+      <g-image background="transparent" fit="contain"  width="90" height="90" src="~/assets/images/technologies/ionic.png" alt="Ionic Logo" />
+      <g-image background="transparent" fit="contain"  width="90" height="90" src="~/assets/images/technologies/js.png" alt="JavaScript Logo" />
   </section>
 </template>
 
