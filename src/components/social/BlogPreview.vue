c632a61ce870388b6b6050ff1c90fde727b15c3f
Andrew Smith <andrew.smith@rolleragency.co.uk>
2019-12-04 14:47:23 +0000

    wip

--- src/components/social/BlogPreview.vue
+++ src/components/social/BlogPreview.vue
@@ -3,7 +3,7 @@
   <div class="contact__blog">
     <h2 class="contact__blog-preview__top-title">Latest from my blog</h2>
     <a href="https://thisdeveloperslife.wordpress.com/" target="_blank" class="contact__blog-preview__url">thisdeveloperslife.wordpress.com</a>
-    <carousel v-if="blogPosts" :spacePadding="10" :spacePaddingMaxOffsetFactor="10" :centerMode="true">
+    <carousel v-if="blogPosts">
       <slide v-for="(post, index) in blogPosts" :key="index" class="contact__blog-preview">
         <article>
           <h4 class="contact__blog-preview__title"><strong><a :href=post.url>{{ post.title }}</a></strong></h4>
@@ -21,7 +21,7 @@ import { Component, Vue, Prop, Mixins } from "vue-property-decorator";
 import axios from 'axios';
 import * as moment from 'moment';
 const { Carousel, Slide } = require('vue-carousel');
-
+  
 @Component({
    components: {
       Carousel,
@@ -29,17 +29,17 @@ const { Carousel, Slide } = require('vue-carousel');
    }
 })
 export default class BlogPreview extends Vue {
-  blogPosts: Array<any> = [];
+  blogPosts: Array<BlogPost> = [];
   async mounted() {
      try {
         let response = await axios.get('https://public-api.wordpress.com/rest/v1.1/sites/117679029/posts/');
-        this.blogPosts = response.data.posts.map((post: any) => {
+        this.blogPosts = response.data.posts.map((post: any): BlogPost => {
           return {
             title: post.title.replace(/<\/?[^>]+>/gi, '').replace(/&amp;/g, '&'),
             url: post.URL,
             intro: post.excerpt.replace(/<\/?[^>]+>/gi, '').replace(/&amp;/g, '&').replace('[&hellip;]', '...'),
             postDate: moment(post.date).format('MMMM Do YYYY, h:mma')
-          }
+          } as BlogPost;
         });
      } catch (e) {
        console.log(e);
--- src/vue-shims.d.ts
+++ src/vue-shims.d.ts
@@ -2,3 +2,10 @@ declare module "*.vue" {
   import Vue from "vue";
   export default Vue;
 }
+
+interface BlogPost {
+  title: string;
+  url: string;
+  intro: string;
+  postDate: string;
+}
