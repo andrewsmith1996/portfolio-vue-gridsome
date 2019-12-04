d342e94c49ae20153e5e990e6e46d30e49458d39
Andrew Smith <andrew.smith@rolleragency.co.uk>
2019-12-04 16:29:28 +0000

    ff

--- src/components/technologies/Technologies.vue
+++ src/components/technologies/Technologies.vue
@@ -1,12 +1,12 @@
 <template>
   <section class="technologies">
-      <g-image @mouseenter="setActive(1)" :class="{'active': technologyActive(1)}" background="transparent" fit="contain"  width="125" height="125" src="~/assets/images/technologies/mongodb.png" alt="MongoDB Logo" />
-      <g-image @mouseenter="setActive(2)" :class="{'active': technologyActive(2)}" background="white" fit="contain" width="125" height="125" src="~/assets/images/technologies/react.png" alt="react.js Logo" />
-      <g-image @mouseenter="setActive(3)" :class="{'active': technologyActive(3)}" background="transparent" fit="contain"  width="125" height="125" src="~/assets/images/technologies/node.png" alt="Node.js Logo" />
-      <g-image @mouseenter="setActive(4)" :class="{'active': technologyActive(4)}" background="transparent" fit="contain" width="125" height="125" src="~/assets/images/technologies/vue.png" alt="Vue.js Logo" />
-      <g-image @mouseenter="setActive(5)" :class="{'active': technologyActive(5)}" background="transparent" fit="contain"  width="125" height="125" src="~/assets/images/technologies/typescript.png" alt="Typescript Logo" />
-      <g-image @mouseenter="setActive(6)" :class="{'active': technologyActive(6)}" background="transparent" fit="contain"  width="125" height="125" src="~/assets/images/technologies/ionic.png" alt="Ionic Logo" />
-      <g-image @mouseenter="setActive(7)" :class="{'active': technologyActive(7)}" background="transparent" fit="contain"  width="125" height="125" src="~/assets/images/technologies/js.png" alt="JavaScript Logo" />
+      <g-image @mouseenter="number = 1" :class="{'active': number === 1}" background="transparent" fit="contain"  width="125" height="125" src="~/assets/images/technologies/mongodb.png" alt="MongoDB Logo" />
+      <g-image @mouseenter="number = 2" :class="{'active': number === 2}" background="white" fit="contain" width="125" height="125" src="~/assets/images/technologies/react.png" alt="react.js Logo" />
+      <g-image @mouseenter="number = 3" :class="{'active': number === 3}" background="transparent" fit="contain"  width="125" height="125" src="~/assets/images/technologies/node.png" alt="Node.js Logo" />
+      <g-image @mouseenter="number = 4" :class="{'active': number === 4}" background="transparent" fit="contain" width="125" height="125" src="~/assets/images/technologies/vue.png" alt="Vue.js Logo" />
+      <g-image @mouseenter="number = 5" :class="{'active': number === 5}" background="transparent" fit="contain"  width="125" height="125" src="~/assets/images/technologies/typescript.png" alt="Typescript Logo" />
+      <g-image @mouseenter="number = 6" :class="{'active': number === 6}" background="transparent" fit="contain"  width="125" height="125" src="~/assets/images/technologies/ionic.png" alt="Ionic Logo" />
+      <g-image @mouseenter="number = 7" :class="{'active': number === 7}" background="transparent" fit="contain"  width="125" height="125" src="~/assets/images/technologies/js.png" alt="JavaScript Logo" />
   </section>
 </template>
 
@@ -16,14 +16,6 @@ import { Component, Vue, Prop, Mixins } from "vue-property-decorator";
 @Component
 export default class Technologies extends Vue {
   number: number | null = null;
-  
-  setActive(id: number) {
-    this.number = id;
-  }
-
-  technologyActive(id: number){
-    return this.number == id;
-  }
 }
 </script>
 
